b17c2bb65ab5ad7d375e7ad32f3d10ade1206043
tmo macbook <torrel.moseley@leverege.com>
2021-07-18 14:30:07 -0400

    Add comments regarding where I'm blocked for not knowing Typescript.

--- frontend/src/PatientList/PatientList.tsx
+++ frontend/src/PatientList/PatientList.tsx
@@ -13,16 +13,24 @@ import {
 export const PatientList: FunctionComponent = () => {
   const [patients, setPatients] = useState<Record<string, any>[]>([]);
 
+  console.log('patients[0]: ', patients[0])
   useEffect(() => {
     (async function getPatients() {
       try {
         const resp = await axios.get("/api/patients");
+        // Can sort response here, but I'm getting type errors I'm not familiar with for not
+        // knowing Typescript.
         setPatients(resp.data);
       } catch (e) {
         console.error(e?.response?.data?.message);
       }
     })();
   }, []);
+
+  // const onClick = (e) => {
+  //   console.log('e: ', e); TypeScript error Parameter 'e' implicitly has an 'any' type. TS7006
+  //                            I'm not familiar with resolving this since I don't know Typescript.
+  // };
 
   return (
     <div style={{ height: "100%" }}>
@@ -43,7 +51,7 @@ export const PatientList: FunctionComponent = () => {
                 <TableCell>{patient.name}</TableCell>
                 <TableCell>{patient.mrn}</TableCell>
                 <TableCell>{patient.dob}</TableCell>
-                <TableCell>{"Add Stars Here"}</TableCell>
+                <TableCell /*onClick={onClick}*/>{patient.starred ? <span>&#9733;</span> : <span>&#9734;</span> }</TableCell>
               </TableRow>
             ))}
           </TableBody>
